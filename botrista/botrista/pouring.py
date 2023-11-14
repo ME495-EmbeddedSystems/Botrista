6928e1af8b9fbcf21054e1b609023c62a3671c8a
kyle <kylewang239@gmail.com>
2023-11-13 20:20:00 -0600

    Worked on pouring node: -Created node frame -Created and tested spiral path building
    TODO:
    -Create launch file and update package files
    -Interface with movitapi to get trajectory
    -Test in simulation
    -Test irl
    -Fix start point input to use tf or april tag home position
    -Update service to use correct callback group

--- botrista/botrista/pouring.py
+++ botrista/botrista/pouring.py
@@ -3,7 +3,10 @@ from rclpy.node import Node
 import math
 import numpy as np
 import time
+<<<<<<< HEAD
 from rclpy.callback_groups import ReentrantCallbackGroup
+=======
+>>>>>>> 6daa95b (Worked on pouring node:)
 
 # Object Importing
 from moveit_wrapper.moveitapi import MoveItApi
@@ -16,15 +19,20 @@ from std_srvs.srv import Empty
 class Pouring(Node):
     def __init__(self):
         super().__init__(node_name="pouring")
+<<<<<<< HEAD
         self.path = None
         self.get_logger().warn("Pouring started")
         self.cb = ReentrantCallbackGroup()
+=======
+        self.get_logger().warn("Pouring started")
+>>>>>>> 6daa95b (Worked on pouring node:)
 
         # Moveit Wrapper Object
         self.moveit = MoveItApi(self,
                                 "panda_link0",
                                 "panda_hand_tcp",
                                 "panda_manipulator",
+<<<<<<< HEAD
                                 "joint_states")
 
         # Creating Services
@@ -57,11 +65,26 @@ class Pouring(Node):
     async def execute_callback(self, request, response):
         # Temporary
         self.moveit.execute_trajectory(self.path)
+=======
+                                "panda_gripper/joint_states")
+
+        # Creating Services
+        self.pour_client = self.create_client(Empty,
+                                              "pour_kettle",
+                                              10,
+                                              self.pour_callback)
+
+        # TODO: get april tag home position and use that
+
+    def pour_callback(self, request, response):
+        # TODO: Fill in
+>>>>>>> 6daa95b (Worked on pouring node:)
         return response
 
 
 def main(args=None):
     rclpy.init(args=args)
+<<<<<<< HEAD
     node = Pouring()
     rclpy.spin(node)
     rclpy.shutdown()
@@ -72,10 +95,24 @@ def get_spiral_waypoints(startPoint: Point,
                          maxRadius: float,
                          loops: float,
                          endStart: bool = False) -> list[Pose]:
+=======
+    Pouring = Pouring()
+    rclpy.spin(Pouring)
+    rclpy.shutdown()
+
+
+def get_spiral_waypoints(start: Point,
+                         numPoints: int,
+                         a: float,
+                         b: float,
+                         loops: float,
+                         flipStart: bool = False) -> list[Pose]:
+>>>>>>> 6daa95b (Worked on pouring node:)
     """
     Create a spiral path given parameters
 
     Arguments:
+<<<<<<< HEAD
         startPoint (geometry_msgs/Point) -- Starting point
         numPoints (int) -- number of points used to build the path
         maxRadius (float) -- distance from end of spiral to origin in cm
@@ -83,6 +120,16 @@ def get_spiral_waypoints(startPoint: Point,
 
     Keyword Arguments:
         endStart (bool) -- Start at the end of the spiral instead of the center (default: {False})
+=======
+        start (geometry_msgs/Point) -- Starting position
+        numPoints (int) -- number of points used to build the path
+        a (float) -- distance of the starting point from the origin
+        b (float) -- distance between turns of the spiral
+        loops -- number of loops for the spiral to go through
+
+    Keyword Arguments:
+        flipStart (bool) -- Start at the end of the spiral instead of the center (default: {False})
+>>>>>>> 6daa95b (Worked on pouring node:)
 
     Returns:
         A list of waypoints
@@ -91,6 +138,7 @@ def get_spiral_waypoints(startPoint: Point,
     count = 0
     thTotal = loops*2*math.pi
     thStep = thTotal/numPoints
+<<<<<<< HEAD
     b = maxRadius/2/math.pi/loops
 
     # Create poses for each point along the spiral
@@ -113,5 +161,25 @@ def get_spiral_waypoints(startPoint: Point,
 
     if endStart:
         poseList.reverse()
+=======
+
+    # A list of tuples that contains r and theta
+    polarCoords = []
+    while count < numPoints:
+        polarCoords.append(((count*thStep)*b+a, count*thStep))
+        count += 1
+
+    poseList = []
+    # Convert polar coordinates into cartesian and add in start offset
+    for coord in polarCoords:
+        x = coord[0]*math.cos(coord[1]) + start.x
+        y = coord[0]*math.sin(coord[1]) + start.y
+        poseList.append(Pose(position=Point(x=x,
+                                            y=y,
+                                            z=start.z)))
+
+    if flipStart:
+        return poseList.reverse
+>>>>>>> 6daa95b (Worked on pouring node:)
 
     return poseList
