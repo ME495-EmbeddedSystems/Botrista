ca36ba3a0de3c0118985231debe4a05c33a97e72
Carter DiOrio <carterdiorio2024@u.northwestern.edu>
2023-11-14 20:43:38 -0600

    Added localizer node that localizes camera to the robot

--- botrista/botrista/camera_localizer.py
+++ botrista/botrista/camera_localizer.py
@@ -1,6 +1,7 @@
 from rclpy.node import Node
 import rclpy
 <<<<<<< HEAD
+<<<<<<< HEAD
 from std_msgs.msg import Header
 from rclpy.duration import Duration
 from tf2_ros import TransformListener, Buffer, TransformBroadcaster
@@ -54,17 +55,59 @@ class CameraLocalizer(Node):
 
 def camera_localizer_entry(args=None):
 =======
+=======
+from std_msgs.msg import Header
+from rclpy.duration import Duration
+from tf2_ros import TransformListener, Buffer, TransformBroadcaster
+from geometry_msgs.msg import TransformStamped, Transform, Vector3
+>>>>>>> 460e438 (Added localizer node that localizes camera to the robot)
 
 
 class CameraLocalizer(Node):
+    """
+    Localizes the ceiling mounted d435i camera to the robot
+    """
+
     def __init__(self):
         super().__init__('camera_localizer')
 
-        pass
+        # create transform listener and buffer
+        self.buffer = Buffer()
+        self.transform_listener = TransformListener(self.buffer, self, qos=10)
+        self.transform_broadcaster = TransformBroadcaster(self, qos=10)
+
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+    def timer_callback(self):
+        try:
+            # find transform to localizer tag
+            localizer_tag_to_franka_tf = TransformStamped()
+            localizer_tag_to_franka_tf.header = Header(
+                stamp=self.get_clock().now().to_msg(),
+                frame_id='camera_localizer_tag'
+            )
+
+            localizer_tag_to_franka_tf.transform = Transform(
+                translation=Vector3(
+                    x=0.555,
+                    y=0.302
+                )
+            )
+
+            localizer_tag_to_franka_tf.child_frame_id = "franka"
+            self.transform_broadcaster.sendTransform(
+                localizer_tag_to_franka_tf)
+
+        except Exception as e:
+            pass
 
 
+<<<<<<< HEAD
 def main(args=None):
 >>>>>>> 5d7f16f (Added launch file that starts the realsense node for both)
+=======
+def camera_localizer_entry(args=None):
+>>>>>>> 460e438 (Added localizer node that localizes camera to the robot)
     rclpy.init(args=args)
     camera_localizer = CameraLocalizer()
     rclpy.spin(camera_localizer)
--- botrista/launch/realsense.launch.py
+++ botrista/launch/realsense.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
 <<<<<<< HEAD
+<<<<<<< HEAD
 from launch_ros.actions import Node, SetRemap
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import IncludeLaunchDescription, GroupAction
@@ -8,6 +9,11 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import IncludeLaunchDescription
 >>>>>>> 5d7f16f (Added launch file that starts the realsense node for both)
+=======
+from launch_ros.actions import Node, SetRemap
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription, GroupAction
+>>>>>>> 460e438 (Added localizer node that localizes camera to the robot)
 from launch.substitutions import PathJoinSubstitution
 
 
@@ -17,10 +23,17 @@ from launch.substitutions import PathJoinSubstitution
 def generate_launch_description():
     return LaunchDescription([
 <<<<<<< HEAD
+<<<<<<< HEAD
         GroupAction(actions=[
             SetRemap(src='/d435i/color/image_raw',
                      dst='/image_raw'),
             SetRemap(src='/d435i/color/camera_info', dst='/camera_info'),
+=======
+        GroupAction(actions=[
+            # SetRemap(src='/d435i/color/image_raw/compressed',
+            #          dst='/image_rect/compressed'),
+            # SetRemap(src='/d435i/color/camera_info', dst='/camera_info'),
+>>>>>>> 460e438 (Added localizer node that localizes camera to the robot)
             IncludeLaunchDescription(
                 PathJoinSubstitution([
                     FindPackageShare("realsense2_camera"),
@@ -30,8 +43,11 @@ def generate_launch_description():
                 launch_arguments={
                     'camera_name': 'd435i',
                     'device_type': 'd435i',
+<<<<<<< HEAD
                     'rgb_camera.profile': '1920x1080x6',
                     'enable_depth': 'false',
+=======
+>>>>>>> 460e438 (Added localizer node that localizes camera to the robot)
                 }.items(),
             ),
         ]),
@@ -47,6 +63,7 @@ def generate_launch_description():
         #         'pointcloud.enable': 'true',
         #     }.items(),
         # ),
+<<<<<<< HEAD
         IncludeLaunchDescription(
             PathJoinSubstitution([
                 FindPackageShare("image_proc"),
@@ -54,6 +71,15 @@ def generate_launch_description():
                 "image_proc.launch.py"
             ])
         ),
+=======
+        # IncludeLaunchDescription(
+        #     PathJoinSubstitution([
+        #         FindPackageShare("image_proc"),
+        #         "launch",
+        #         "image_proc.launch.py"
+        #     ])
+        # ),
+>>>>>>> 460e438 (Added localizer node that localizes camera to the robot)
         Node(
             package="apriltag_ros",
             executable="apriltag_node",
@@ -65,6 +91,7 @@ def generate_launch_description():
                     "tag.yaml"
                 ])
             ],
+<<<<<<< HEAD
             # remappings={
             #     'image_rect': 'image_raw',
             # }.items()
@@ -99,4 +126,16 @@ def generate_launch_description():
             }.items(),
         ),
 >>>>>>> 5d7f16f (Added launch file that starts the realsense node for both)
+=======
+            remappings={
+                'image_rect': 'd435i/color/image_raw',
+                'image_rect/compressed': 'd435i/color/image_raw/compressed',
+                'camera_info': 'd435i/color/camera_info',
+            }.items()
+        ),
+        Node(
+            package="botrista",
+            executable="camera_localizer",
+        )
+>>>>>>> 460e438 (Added localizer node that localizes camera to the robot)
     ])
